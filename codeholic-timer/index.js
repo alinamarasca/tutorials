e693eed3728e539ba36bbe7bc6fb44e815a6cd13
HYF <alinamarasca@gmail.com>
2021-07-25 02:12:23 +0200

    replace interval with timeout

--- codeholic-timer/index.js
+++ codeholic-timer/index.js
@@ -11,7 +11,7 @@ const pauseBtn = document.querySelector('.pause-btn');
 const stopBtn = document.querySelector('.stop-btn');
 const resetBtn = document.querySelector('.reset-btn');
 
-let interval;
+let timeout;
 let pause = false;
 let totalSeconds = 0;
 let totalSecondsBackup = 0; 
@@ -44,10 +44,13 @@ function init(){
 pauseBtn.addEventListener('click', ()=>{
     pause = !pause;
     if (pause){
+        
         pauseBtn.innerText = 'Resume';
         } else {
         pauseBtn.innerText = 'Pause';
+        startTimer();
     }
+    
 })
 
 stopBtn.addEventListener('click', ()=>{
@@ -69,8 +72,6 @@ resetBtn.addEventListener('click', () => { //doesn't work
 }
 
 function startTimer(){
-    interval = setInterval(()=> {
-
         if(pause === true){
             return;
         }
@@ -80,57 +81,20 @@ function startTimer(){
         if(totalSeconds <= 0){
             stopTimer();
           }
-    }, 1000)
+    timeout = setTimeout( startTimer, 1000)
 }
 
 function stopTimer(){
-    interval = clearInterval(interval);
+    timeout = clearTimeout(timeout);
 }
 
 function updateInputs(){
     const hours = Math.floor(totalSeconds / 60 / 60);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
-  
     
     hoursCount.textContent = hours;
     minutesCount.textContent = minutes;
     secondesCount.textContent = seconds;
     
 }
-
-
-// const countdown = () =>{
-//     const countDate = new Date('August 10, 2021 00:00:00').getTime();
-//     // console.log(countDate)
-//     const now = new Date().getTime();
-//     //console.log(now);
-//     const gap = countDate - now;
-//     // console.log(gap);
-
-//     //how time works?
-//     const millisecond = 1;
-//     const second = millisecond * 1000;
-//     const minute = second * 60;
-//     const hour = minute * 60;
-//     const day = hour * 24;
-//     // console.log(day)
-
-//     //calc time
-//     const textDay = Math.floor(gap/day);
-//     // console.log(textDay);
-//     const textHour = Math.floor((gap % day)/hour);
-//     // console.log(textHour);
-//     const textMinute = Math.floor((gap % hour)/minute);
-//     const textSecond = Math.floor((gap % minute)/second);
-//     const textMillisecond = Math.floor((gap % second) / millisecond)
-
-//     document.querySelector(".day").innerText = textDay;
-//     document.querySelector(".hour").innerText = textHour;
-//     document.querySelector(".minute").innerText = textMinute;
-//     document.querySelector(".second").innerText = textSecond;
-//     document.querySelector(".millisecond").innerText = textMillisecond;
-//     setTimeout(countdown, 1);
-// }
-// // setTimeout(countdown, 1000);
-// countdown();
